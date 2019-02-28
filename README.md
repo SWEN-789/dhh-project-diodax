fef827102ff17571a80c1adf130cf9a7cd0d7735
Diosdavi Lara <diosdavi@megsoftconsulting.com>
2019-02-28 04:06:45 -0500

    MVP implementation of the speech recognition library

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ A demo for a web-based transcriber of podcasts and other audio files to written 
 
 This project uses [cherrypy](http://www.cherrypy.org/) for the web server, and [jinja2](http://jinja.pocoo.org/docs/dev/) for html rendering. The audio files are transcribed server-side using the [Google Cloud Speech API](https://cloud.google.com/speech-to-text/). 
 
+Example audio files are located on the `/examples` folder.
+
 ## Setting up locally
 
 Make sure you have [Python](https://www.python.org/) and [pip](https://pip.pypa.io/en/stable/installing/) installed.
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import cherrypy
 import os
+import time
+import speech_recognition as sr
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('html'))
 
@@ -7,9 +9,46 @@ env = Environment(loader=FileSystemLoader('html'))
 class Root(object):
     @cherrypy.expose
     def index(self):
-        data_to_show = ['Hello', 'world']
         tmpl = env.get_template('index.html')
-        return tmpl.render(data=data_to_show)
+        return tmpl.render()
+
+    @cherrypy.expose
+    def upload(self, ufile):
+        # Save the file to the directory where app.py is:
+        upload_path = os.path.dirname(__file__)
+
+        # Save the file using the filename sent by the client, with an added timestamp:
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        upload_filename = timestamp + ufile.filename
+
+        upload_file = os.path.normpath(
+            os.path.join(upload_path, upload_filename))
+        size = 0
+        with open(upload_file, 'wb') as out:
+            while True:
+                data = ufile.file.read(8192)
+                if not data:
+                    break
+                out.write(data)
+                size += len(data)
+        upload_success = '''
+        File received successfully.
+        Filename: {}
+        Length: {}
+        Mime-type: {}
+        '''.format(upload_filename, size, ufile.content_type, data)
+        cherrypy.log(upload_success)
+
+        # Now that we have the file saved on the disk, proceed to do the speech processing
+        # use the audio file as the audio source
+        # audio file should be in the .wav format
+        r = sr.Recognizer()
+        with sr.AudioFile(upload_filename) as source:
+            audio = r.record(source)  # read the entire audio file
+            result_text = r.recognize_google(audio)
+
+        tmpl = env.get_template('result.html')
+        return tmpl.render(message=result_text)
 
 
 if __name__ == '__main__':
